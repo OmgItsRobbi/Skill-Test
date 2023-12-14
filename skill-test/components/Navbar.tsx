14fb01e597d7999aece3f6af89353d8edbe6ba34
Robbi <morabitoroberto@gmail.com>
2023-12-14 14:48:16 +0100

    Added navigation bar

---
+++ skill-test/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import TableChartIcon from '@mui/icons-material/TableChart';
+import { useRouter } from 'next/router';
+
+const Navbar: React.FC = () => {
+    const router = useRouter();
+
+    const goToTablePage = () => {
+        router.push('/table'); // Adjust the path based on your folder structure
+    };
+
+    const signOut = () => {
+        // Implement sign-out logic, such as clearing authentication token, etc.
+    router.push('/'); // Adjust the path based on your folder structure
+    };
+
+    return (
+    <AppBar position="static" style={{ backgroundColor: "#20262E", borderBottom: '2px solid #DDDDDD' }}>
+        <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="back" onClick={signOut}>
+                <ArrowBackIcon />
+            </IconButton>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                Sign out
+            </Typography>
+            <IconButton edge="end" color="inherit" aria-label="table" onClick={goToTablePage}>
+                <TableChartIcon />
+            </IconButton>
+        </Toolbar>
+    </AppBar>
+    );
+};
+
+export default Navbar;
--- skill-test/pages/Dashboard.tsx
+++ skill-test/pages/Dashboard.tsx
@@ -1,105 +1,95 @@
 import React, { useEffect, useState } from 'react';
 import { Grid, Paper, Typography } from '@mui/material';
-import {
-    BarChart,
-    Bar,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-    PieChart,
-    Pie,
-    Cell,
-} from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, PieChart, Pie, Cell } from 'recharts';
+import Navbar from '../components/Navbar'
 
 const Dashboard: React.FC = () => {
-const [dashboardData, setDashboardData] = useState({
+  const [dashboardData, setDashboardData] = useState({
     tables: [],
     barChartData: [],
     pieChartData: [],
     anotherPieChartData: [],
     anotherBarChartData: [],
-});
+  });
 
-useEffect(() => {
+  useEffect(() => {
     // Fetch data from mocky.io
     const fetchData = async () => {
-    try {
+      try {
         const response = await fetch('https://run.mocky.io/v3/35f8433d-b1fc-4698-83cc-afed942a5633');
         const data = await response.json();
         console.log('Fetched data:', data);
         setDashboardData(data);
-    } catch (error) {
+      } catch (error) {
         console.error('Error fetching data:', error);
-    }
+      }
     };
 
     fetchData();
-}, []);
+  }, []);
 
-return (
-    <Grid container spacing={2} style={{ paddingTop: 20, paddingLeft: 10, paddingRight: 10 }}>
-      {/* Render Bar Chart */}
+  return (
+    <div>
+      <Navbar />
+      <Grid container spacing={2} style={{ paddingTop: 20, paddingLeft: 10, paddingRight: 10 }}>
         <Grid item xs={12} md={6}>
-        <Paper 
-            elevation={3} 
-            style={{
-                    padding: 20, 
-                    marginBottom: 20 ,
-                    display: 'flex',
-                    flexDirection:'column',
-                    alignItems: 'center',
-                    width: '100%',
-                    maxWidth: 800,
-                    backgroundColor: '#DDDDDD',
-                    border: '3px solid black'
-                    }}>
-            <Typography variant="h6" gutterBottom style={{ color: '#333333', fontFamily: 'Arial, sans-serif', fontSize: 30, fontWeight: 'bold' }}>
-                Bar Chart
-            </Typography>
-            <BarChart
-                width={400}
-                height={300}
-                data={dashboardData.barChartData}
-                margin={{ top: 20, right: 40, left: 10, bottom: 5 }}
-                style={{ backgroundColor: "#EEEEEE", border: '1px solid black' }}
-                
-                >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="category" />
-                <YAxis />
-                <Tooltip />
-                {/* Modify colors here */}
-                <Bar dataKey="value" fill="#FF5733" />
-            </BarChart>
-        </Paper>
-        </Grid>
-
-      {/* Render Pie Chart */}
-        <Grid item xs={12} md={6}>
-        <Paper 
+          <Paper
             elevation={3}
             style={{
-                padding: 20, 
-                marginBottom: 20 ,
-                display: 'flex',
-                flexDirection:'column',
-                alignItems: 'center',
-                width: '100%',
-                maxWidth: 800,
-                backgroundColor: '#DDDDDD',
-                border: '3px solid black'
-                }}>
+              padding: 20,
+              marginBottom: 20,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '100%',
+              maxWidth: 800,
+              backgroundColor: '#DDDDDD',
+              border: '3px solid black',
+            }}
+          >
             <Typography variant="h6" gutterBottom style={{ color: '#333333', fontFamily: 'Arial, sans-serif', fontSize: 30, fontWeight: 'bold' }}>
-                Pie Chart
+              Bar Chart
             </Typography>
-            <PieChart 
-                width={400} 
-                height={300}
-                style={{ backgroundColor: "#EEEEEE", border: '1px solid black' }}
-                >
-            <Pie
+            <BarChart
+              width={400}
+              height={300}
+              data={dashboardData.barChartData}
+              margin={{ top: 20, right: 40, left: 10, bottom: 5 }}
+              style={{ backgroundColor: "#EEEEEE", border: '1px solid black' }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="category" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="value" fill="#FF5733" />
+            </BarChart>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Paper
+            elevation={3}
+            style={{
+              padding: 20,
+              marginBottom: 20,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '100%',
+              maxWidth: 800,
+              backgroundColor: '#DDDDDD',
+              border: '3px solid black',
+            }}
+          >
+            <Typography variant="h6" gutterBottom style={{ color: '#333333', fontFamily: 'Arial, sans-serif', fontSize: 30, fontWeight: 'bold' }}>
+              Pie Chart
+            </Typography>
+            <PieChart
+              width={400}
+              height={300}
+              style={{ backgroundColor: "#EEEEEE", border: '1px solid black' }}
+            >
+              <Pie
                 dataKey="value"
                 isAnimationActive={false}
                 data={dashboardData.pieChartData}
@@ -107,39 +97,39 @@ return (
                 cy="50%"
                 outerRadius={100}
                 label
-            >
-            <Cell key="cell-0" fill="#FFD369" />
-            <Cell key="cell-1" fill="#FF0000" />
-            <Cell key="cell-2" fill="#03506F" />
-            </Pie>
+              >
+                <Cell key="cell-0" fill="#FFD369" />
+                <Cell key="cell-1" fill="#FF0000" />
+                <Cell key="cell-2" fill="#03506F" />
+              </Pie>
             </PieChart>
-        </Paper>
+          </Paper>
         </Grid>
 
-      {/* Render Another Pie Chart */}
         <Grid item xs={12} md={6}>
-        <Paper 
+          <Paper
             elevation={3}
             style={{
-                padding: 20, 
-                marginBottom: 20 ,
-                display: 'flex',
-                flexDirection:'column',
-                alignItems: 'center',
-                width: '100%',
-                maxWidth: 800,
-                backgroundColor: '#DDDDDD',
-                border: '3px solid black'
-                }}>
+              padding: 20,
+              marginBottom: 20,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '100%',
+              maxWidth: 800,
+              backgroundColor: '#DDDDDD',
+              border: '3px solid black',
+            }}
+          >
             <Typography variant="h6" gutterBottom style={{ color: '#333333', fontFamily: 'Arial, sans-serif', fontSize: 30, fontWeight: 'bold' }}>
-                Another Pie Chart
+              Another Pie Chart
             </Typography>
-            <PieChart 
-                width={400} 
-                height={300}
-                style={{ backgroundColor: "#EEEEEE", border: '1px solid black' }}
-                >
-            <Pie
+            <PieChart
+              width={400}
+              height={300}
+              style={{ backgroundColor: "#EEEEEE", border: '1px solid black' }}
+            >
+              <Pie
                 dataKey="value"
                 isAnimationActive={false}
                 data={dashboardData.anotherPieChartData}
@@ -147,52 +137,51 @@ return (
                 cy="50%"
                 outerRadius={100}
                 label
-            >
-            <Cell key="cell-0" fill="#FFD369" />
-            <Cell key="cell-1" fill="#FF0000" />
-            <Cell key="cell-2" fill="#03506F" />
-            </Pie>
+              >
+                <Cell key="cell-0" fill="#FFD369" />
+                <Cell key="cell-1" fill="#FF0000" />
+                <Cell key="cell-2" fill="#03506F" />
+              </Pie>
             </PieChart>
-        </Paper>
+          </Paper>
         </Grid>
 
-      {/* Render Another Bar Chart */}
         <Grid item xs={12} md={6}>
-        <Paper 
+          <Paper
             elevation={3}
             style={{
-                padding: 20, 
-                marginBottom: 20 ,
-                display: 'flex',
-                flexDirection:'column',
-                alignItems: 'center',
-                width: '100%',
-                maxWidth: 800,
-                backgroundColor: '#DDDDDD',
-                border: '3px solid black'
-                }}
-            >
+              padding: 20,
+              marginBottom: 20,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '100%',
+              maxWidth: 800,
+              backgroundColor: '#DDDDDD',
+              border: '3px solid black',
+            }}
+          >
             <Typography variant="h6" gutterBottom style={{ color: '#333333', fontFamily: 'Arial, sans-serif', fontSize: 30, fontWeight: 'bold' }}>
-                Another Bar Chart
+              Another Bar Chart
             </Typography>
             <BarChart
-                width={400}
-                height={300}
-                data={dashboardData.anotherBarChartData}
-                margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-                style={{ backgroundColor: "#EEEEEE", border: '1px solid black' }}
+              width={400}
+              height={300}
+              data={dashboardData.anotherBarChartData}
+              margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+              style={{ backgroundColor: "#EEEEEE", border: '1px solid black' }}
             >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            {/* Modify colors here */}
-            <Bar dataKey="value" fill="#82ca9d" />
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="value" fill="#82ca9d" />
             </BarChart>
-        </Paper>
+          </Paper>
         </Grid>
-    </Grid>
-);
+      </Grid>
+    </div>
+  );
 };
 
 export default Dashboard;
